84883e5f46e58ab68869f058b264daf8e99cf9f3
Yura Nosenko <yuranos87@gmail.com>
2018-01-25 18:37:12 +0200

    Init commit

---
+++ src/main/java/com/yuranos/raml/ramlbasedapplication/RamlBasedApplication.java
@@ -0,0 +1,12 @@
+package com.yuranos.raml.ramlbasedapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamlBasedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RamlBasedApplication.class, args);
+	}
+}
